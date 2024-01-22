b269f5c0d45392ea753dbae546a77c772231a2c9
AbdulAmir1 <abdulamir0@gmail.com>
2024-01-22 13:10:19 +0300

    compareP1CodeToP2Guess() method modified to check for the number of matched colors

--- js/script.js
+++ js/script.js
@@ -1,32 +1,47 @@
 const NUMBER_OF_HOLES = 4
 let player1CodePattern = ['red', 'blue', 'yellow', 'green']
+// let resultArray = []
 
 const compareP1CodeToP2Guess = (p2Guess) => {
-  let i = 0
+  let j = 0
+  let colorMatch = true
+  let resultArray = []
+
   console.assert(
     p2Guess.length === NUMBER_OF_HOLES,
     'p2Guess array length is correct'
   )
-  while (p2Guess[i] === player1CodePattern[i]) {
-    i++
-    if (i > NUMBER_OF_HOLES - 1) {
-      return true
+
+  for (let i = 0; i < player1CodePattern.length; i++) {
+    if (p2Guess[i] === player1CodePattern[i]) {
+      resultArray.push(2)
+    } else if (player1CodePattern.includes(p2Guess[i])) {
+      resultArray.push(1)
+    } else {
+      resultArray.push(0)
     }
   }
 
-  return false
+  while (resultArray[j] === 2) {
+    j++
+    if (j > NUMBER_OF_HOLES - 1) {
+      return { playerWin: true, roundGuessResult: resultArray }
+    }
+  }
+
+  return { playerWin: false, roundGuessResult: resultArray }
 }
 
 const evaluatePlayerGuessPattern = (player2Guess) => {
   let player2GuessedRight
-  if (compareP1CodeToP2Guess(player2Guess)) {
+  const roundResult = compareP1CodeToP2Guess(player2Guess)
+
+  if (roundResult.playerWin) {
     player2GuessedRight = true
   } else {
     player2GuessedRight = false
   }
 
-  updateGameboardUIDOM(player2GuessedRight, player2Guess)
+  updateGameboardUIDOM(roundResult.roundGuessResult)
   return player2GuessedRight
 }
-
-const updateGameboardUIDOM = (player2GuessedRight, player2Guess) => {}
