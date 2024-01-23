4de7a5901052539c88f9ee092b96b9fb3ca66a02
AbdulAmir1 <abdulamir0@gmail.com>
2024-01-23 11:16:05 +0300

    game is modified: main.html has one round result section and js classes are used. gameBoardWithClasses.js created

--- js/script.js
+++ js/script.js
@@ -1,6 +1,17 @@
 const NUMBER_OF_HOLES = 4
+const allColors = ['red', 'blue', 'yellow', 'green', 'cyan', 'pink', '#ffdc79']
 let player1CodePattern = ['red', 'blue', 'yellow', 'green']
 // let resultArray = []
+
+const setPlayer1CodePattern = () => {
+  player1CodePattern = []
+  for (let i = 0; i < 4; i++) {
+    const randomColorIndex = Math.floor(Math.random() * allColors.length)
+    player1CodePattern.push(allColors[randomColorIndex])
+  }
+
+  console.log('Player 1 colors ++++>> ', player1CodePattern)
+}
 
 const compareP1CodeToP2Guess = (p2Guess) => {
   let j = 0
