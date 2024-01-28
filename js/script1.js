0c875f4271d534120cec77733ae989ff582b0d0d
AbdulAmir1 <abdulamir0@gmail.com>
2024-01-28 08:10:03 +0300

    keypress event set for start button in index.html

--- js/script1.js
+++ js/script1.js
@@ -1,20 +1,30 @@
-// methods to update UI DOM
-document.querySelector('#startGameButton').addEventListener('click', () => {
-  userNameDialog.open = true
-  userNameDialog.style.animationPlayState = 'running'
-})
+// methods to update UI DOM (index.html)
 
 const getPlayerNameDialog = document.querySelector('#userNameDialog')
 const userNameDialog = document.querySelector('#userNameDialog')
 const userNameInput = document.querySelector('#player')
 
-document.querySelector('#okButton').addEventListener('click', () => {
+document.querySelector('#startGameButton').addEventListener('click', () => {
+  userNameDialog.open = true
+  userNameDialog.style.animationPlayState = 'running'
+  userNameInput.focus()
+})
+
+const startGame = () => {
   setPlayerName(userNameInput.value)
   location.href = 'main.html'
   console.log('Your name ==', userNameInput.value)
   userNameDialog.style.animationName = 'slideUp'
   userNameDialog.style.animationPlayState = 'running'
   userNameDialog.open = false
+}
+
+document.querySelector('#okButton').addEventListener('click', startGame)
+
+userNameInput.addEventListener('keypress', (e) => {
+  if (e.key === 'Enter') {
+    startGame()
+  }
 })
 
 const setPlayerName = (playerName) => {
